c918d4857b31ca2f19f0e86120c6598db813eecf
YG1ee <fanfolderkim@live.co.kr>
2016-04-06 19:50:18 +0900

    pointer practice_1

--- firstGithubSln/firstGithubSln/firstGithubSln.cpp
+++ firstGithubSln/firstGithubSln/firstGithubSln.cpp
@@ -3,10 +3,22 @@
 
 #include "stdafx.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	int i = 0, j = 0;
+int main() {
+	int i, j;
+	int *ptr;
+	ptr = &i;
+	*ptr = 10;
+	j = *ptr;
 
-    return 0;
+	printf("i = %d\n", i);
+	printf("&i = %d\n", &i);
+	printf("j = %d\n", j);
+	printf("&j = %d\n", &j);
+	printf("*ptr = %d\n", *ptr);
+	printf("ptr = %d\n", ptr);
+
+	system("pause");
+	return 0;
 }
