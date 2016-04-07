2bcfe87952a785cada77f45cf9dc00464415ee49
YG1ee <fanfolderkim@live.co.kr>
2016-04-07 19:26:57 +0900

    콘솔 입출력 코드 완료 - 파일 입출력으로 고치면 됨

--- forAccountBook/forAccountBook/accountProgram.c
+++ forAccountBook/forAccountBook/accountProgram.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+// #pragma warning(disable:4996)
 
 int main() {
 	int i, firstRow, lastRow;
-	char firstColumn, secondColumn;
+	char firstColumn[3], secondColumn[3];
 
 	printf("곱할 첫 번째 열을 입력하세요.\n 예: A, B, F 등등: ");
-	scanf("%c", &firstColumn);
+	scanf_s("%s", firstColumn, sizeof(firstColumn));
 	printf("곱할 두 번째 열을 입력하세요.\n 예: A, B, F 등등: ");
-	scanf("%c", &secondColumn);
+	scanf_s("%s", secondColumn, sizeof(secondColumn));
 
 	printf("시작하는 행의 숫자를 입력하세요.\n 예: 4, 6, 10 등등: ");
 	scanf("%d", &firstRow);
@@ -16,12 +17,12 @@ int main() {
 	
 	printf("=");
 	for (i = firstRow; i < lastRow+1; i++) {
-		pritnf("$%c%d*%c%d", firstColumn, i, secondColumn, i);
+		printf("$%s%d*%s%d", firstColumn, i, secondColumn, i);
 		if (i != lastRow)
-			pritnf("+");
+			printf("+");
 	}
 
-	printf("끝났습니다. 메모장에 적힌 내용을 그대로 셀에 복사하세요!\n");
+	printf("\n끝났습니다. 메모장에 적힌 내용을 그대로 셀에 복사하세요!\n");
 
 	return 0;
 }
