43d4e74043c098823bba25b013ea74cf7e439cf8
YG1ee <fanfolderkim@live.co.kr>
2016-05-10 10:28:20 +0900

    added traversal functions

--- binaryTree/binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree/binaryTree.cpp
@@ -18,6 +18,30 @@ treeNode* makeRootNode(char data, treeNode* leftNode, treeNode* rightNode) {
 	return root;
 }
 
+void preorder(treeNode* root) {
+	if (root) {
+		printf("%c", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void inorder(treeNode* root) {
+	if (root) {
+		inorder(root->left);
+		printf("%c", root->data);
+		inorder(root->right);
+	}
+}
+
+void postorder(treeNode* root) {
+	if (root) {
+		postorder(root->left);
+		postorder(root->right);
+		printf("%c", root->data);
+	}
+}
+
 int main() {
 	
 
