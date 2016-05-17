b55eb89d199b1e6deb3f679fe565e861bb4bffe9
YG1ee <fanfolderkim@live.co.kr>
2016-05-17 09:24:35 +0900

    traversal for binaryTree

--- binaryTree/binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree/binaryTree.cpp
@@ -43,8 +43,26 @@ void postorder(treeNode* root) {
 }
 
 int main() {
-	
+	treeNode* n7 = makeRootNode('D', NULL, NULL);
+	treeNode* n6 = makeRootNode('C', NULL, NULL);
+	treeNode* n5 = makeRootNode('B', NULL, NULL);
+	treeNode* n4 = makeRootNode('A', NULL, NULL);
+	treeNode* n3 = makeRootNode('/', n6, n7);
+	treeNode* n2 = makeRootNode('*', n4, n5);
+	treeNode* n1 = makeRootNode('-', n2, n3);
 
+	printf(" preorder: ");
+	preorder(n1);
+
+	printf("\n inorder: ");
+	inorder(n1);
+
+	printf("\n postorder: ");
+	postorder(n1);
+
+	printf("\n");
+
+	system("pause");
     return 0;
 }
 
