1fd4bf779144d99f47a31d8cc06a3240e6bf46df
YG1ee <fanfolderkim@live.co.kr>
2016-06-13 22:13:03 +0900

    corrected variable name

--- heap/heap/heap.cpp
+++ heap/heap/heap.cpp
@@ -9,19 +9,19 @@
 
 typedef struct { // 힙에 대한 1차원 배열과 힙 원소의 갯수를 구조체로 묶어서 선언
 	int heap[MAX_ELEMENT];
-	int heap_size;
+	int heapSize;
 } heapType;
 
 heapType *createHeap() {
 	heapType *h = (heapType*)malloc(sizeof(heapType));
-	h->heap_size = 0;
+	h->heapSize = 0;
 	return h;
 }
 
 	void insertHeap(heapType *h, int item) { // 힙에 item을 삽입하는 연산
 	int i;
-	h->heap_size += 1;
-	i = h->heap_size;
+	h->heapSize += 1;
+	i = h->heapSize;
 	while ((i != 1) && (item < h->heap[i / 2])) {
 		h->heap[i] = h->heap[i / 2];
 		i /= 2;
@@ -33,11 +33,11 @@ int deleteHeap(heapType *h) { // 힙의 루트를 삭제하여 반환하는 연산
 	int parent, child;
 	int item, temp;
 	item = h->heap[1];
-	temp = h->heap[h->heap_size];
-	h->heap_size = h->heap_size - 1;
+	temp = h->heap[h->heapSize];
+	h->heapSize = h->heapSize - 1;
 	parent = 1;
 	child = 2;
-	while (child <= h->heap_size) {
+	while (child <= h->heapSize) {
 		if ((h->heap[child]) > h->heap[child + 1])
 			child++;
 		if (temp <= h->heap[child])
@@ -55,58 +55,57 @@ int deleteHeap(heapType *h) { // 힙의 루트를 삭제하여 반환하는 연산
 void printHeap(heapType *h) { // 1차원 배열 힙의 내용을 출력하는 연산
 	int i;
 	printf(" Heap: ");
-	for (i = 1; i <= h->heap_size; i++)
+	for (i = 1; i <= h->heapSize; i++)
 		printf("[%d] ", h->heap[i]);
 }
 
 int main() {
-	int main() {
-		int i, /*j, m,*/ n, item;
-		heapType *heap = createHeap();
-		/*srand(time(NULL));
-		for (j = 0; j < 10; j++) {
+	int i, /*j, m,*/ n, item;
+	heapType *heap = createHeap();
+	/*srand(time(NULL));
+	for (j = 0; j < 10; j++) {
 
-		m = rand() % 10 + 1;
-		insertHeap(heap, m);
-		}*/
-		insertHeap(heap, 2);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 4);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 2);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 4);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 9);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 8);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 8);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 10);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 7);
-		printHeap(heap);
-		printf("\n");
-		insertHeap(heap, 9);
-		printHeap(heap);
-		printf("\n");
+	m = rand() % 10 + 1;
+	insertHeap(heap, m);
+	}*/
+	insertHeap(heap, 2);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 4);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 2);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 4);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 9);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 8);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 8);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 10);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 7);
+	printHeap(heap);
+	printf("\n");
+	insertHeap(heap, 9);
+	printHeap(heap);
+	printf("\n");
 
-		n = heap->heapSize;
-		for (i = 0; i < n; i++) {
-			item = deleteHeap(heap);
-			if (i < n)
-				printf("\n delete: [%d] ", item);
-		}
-
-		system("pause");
-		return 0;
+	n = heap->heapSize;
+	for (i = 0; i < n; i++) {
+		item = deleteHeap(heap);
+		if (i < n)
+			printf("\n delete: [%d] ", item);
 	}
+
+	system("pause");
+	return 0;
+}
